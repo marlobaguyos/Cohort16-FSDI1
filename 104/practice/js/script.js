05cf5f022e32472a5e6167eb552a4b1f14daf074
Marlo <marlo.baguyos@outlook.com>
2021-02-09 09:11:12 -0500

    update

---
+++ 104/practice/js/script.js
@@ -0,0 +1,17 @@
+//object literal
+
+const person = {
+    name:"Trae Davis",
+    age:80
+}
+
+console.log(person.name);
+console.log(person.age);
+
+let product={
+    code:"12345",
+    description:"Smart TV",
+    price:256.50
+}
+
+document.write(`<p>${product.description}</p>`)
