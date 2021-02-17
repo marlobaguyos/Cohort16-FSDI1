1ad9d776fdb161974b2a32baf83f43158b71a6cc
Marlo <marlo.baguyos@outlook.com>
2021-02-17 17:34:44 -0500

    update

--- 104/project/js/register.js
+++ 104/project/js/register.js
@@ -8,27 +8,51 @@ const salon={
         open:"8:00 am",
         close:"5:00 pm"
     },
-    pets:[
-        {name:"Scooby",age:50,breed:"Dane",gender:"Male",owner:"Shaggy",phone:"555-555-5555"},
-        {name:"Scrapy",age:40,breed:"Dane",gender:"Male",owner:"Shaggy",phone:"555-555-5555"},
-        {name:"Speedy",age:60,breed:"Mixed",gender:"Male",owner:"Bugs",phone:"444-444-4444"},
-        {name:"Stunky",age:22,breed:"pug",gender:"Female",owner:"Marlo",phone:"333-333-3333"},
-        {name:"Spunky",age:17,breed:"corky",gender:"FeMale",owner:"Erica",phone:"222-222-2222"}
-    ]
+    pets:[]
 }
 document.getElementById("footer-text").innerHTML=`
     <p>${salon.name}</p>
     <p>${salon.address.street}, ${salon.address.number}</p>
     <p>It's open from ${salon.hours.open} to ${salon.hours.close}</p>
-`
+`;
 
-for(var i=0;i<5;i++){
-    var numPets = `
-        <p>Number of Pet registered: ${salon.pets.length}</p>
-        `
-    document.getElementById("total").innerHTML=numPets;
-    var petName=`
-        <p>${salon.pets[i].name}</p>
-        `
-    document.getElementById("pets").innerHTML+=petName
-}
+// for(var i=0;i<5;i++){
+//     var numPets = `
+//         <p>Number of Pet registered: ${salon.pets.length}</p>
+//         `
+//     document.getElementById("total").innerHTML=numPets;
+//     var petName=`
+//         <p>${salon.pets[i].name}</p>
+//         `
+//     document.getElementById("pets").innerHTML+=petName
+// }
+
+//create the pet class
+    class Pet{
+        constructor(name,age,breed,gender,service,owner,phone){
+            this.name=name;
+            this.age=age;
+            this.breed=breed;
+            this.gender=gender;
+            this.service=service;
+            this.owner=owner;
+            this.phone=phone;
+        }
+    }
+
+//create pets using the constructor
+var scooby=new Pet("Scooby",50,"Dane","Male","Full Service","Shaggy","555-555-5555");
+var scrapy=new Pet("Scrapy",40,"Dane","Male","Shower","Shaggy","555-555-5555");
+var speedy=new Pet("Speedy",80,"Mixed","Male","Nails cut","Bunny","333-333-3333");
+console.log(scooby,scrapy,speedy);
+
+//push the pets into the array
+//salon.pets=[scooby,scrapy,speedy];
+salon.pets.push(scooby);
+salon.pets.push(scrapy);
+salon.pets.push(speedy);
+console.log(salon.pets);
+    //create the function register
+    function register(){
+        
+    }
