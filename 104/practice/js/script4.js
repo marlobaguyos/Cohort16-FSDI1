3789c90a358c60ab73f06b7dc5f41246590bb019
Marlo <marlo.baguyos@outlook.com>
2021-02-17 14:19:49 -0500

    update

--- 104/practice/js/script4.js
+++ 104/practice/js/script4.js
@@ -1,38 +1,17 @@
-var products=["Mac M1", "Magic Keyboard", "Magic Mouse", "Backpack"];
-var prices = [1500,150,100,100];
-var item1={
-    product:"Mac M1",
-    price:1500
+class Item {
+    constructor(a, b){
+        this.product=a;
+        this.price=b;
+    }
 }
-var item2={
-    product:"Magis Keyboard",
-    price: 150
-}
-var item3={
-    product:"Magic Mouse",
-    price:100
-}
-var item4={
-    product:"Backpack",
-    price:100
-}
-var item5={
-    product:"Echo dot",
-    price:150
-}
-var items=[item1,item2,item3,item4,item5]
-
-//travel the array
-for (var i=0;i<products.length;i++){
-    console.log(`${products[i]} ---- ${prices[i]}`);
-}
-
-//display the total of my cart
-
-//var total=preces[0] + prices[1] + prices[2] + prices[3] + prices [4]
+var item1=newItem("Mac M1",1500);
+var item2=newItem("Magic Mouse",150);
+var item3=newItem("Magic Keyboard",120);
+var item4=newItem("Backpack",100);
+var item5=newItem("Echo dot",150);
 
 var total = 0;
-for (var j=0;j<prices.length;j++){
-    total = prices[j] + total;
+for (var j=0;j<items.length;j++){
+    total = items[j].price + total;
 }
 console.log(`The total is: ${total}`);
