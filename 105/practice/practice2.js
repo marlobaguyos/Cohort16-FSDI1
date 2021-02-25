13a2623a13c81c50b596a98adc162001ea19cebd
Marlo <marlo.baguyos@outlook.com>
2021-02-25 13:40:23 -0500

    update

--- 105/practice/practice2.js
+++ 105/practice/practice2.js
@@ -1,6 +1,9 @@
 console.log("Practice 2");
 //document.querySelector('main).display="none";
-$('main').hide();
-$('main').show();
-$('nav a:fist').hide();
+jQuery('main').hide();
+jQuery('main').show();
+jQuery('nav a:fist').hide();
 //hide the last input
+jQuery('form input:last').hide();
+var copy=$('main').clone();
+jQuery('main').append(copy);
