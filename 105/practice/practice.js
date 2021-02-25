9654cbc3dfbb117d183c91ead1c3e923ef053fdd
Marlo <marlo.baguyos@outlook.com>
2021-02-25 11:02:17 -0500

    update

--- 105/practice/practice.js
+++ 105/practice/practice.js
@@ -1,3 +1,13 @@
 //atributes
-var links = document.getElementById('a');
+var links = document.getElementsByTagName('a');
 console.log(links);
+links[0].setAttribute('target','blank');
+links[1].setAttribute('class','bordered');
+
+//create html elements
+var h1=document.createElement('h1');
+var text=document.createTextNode("Hello World");
+h1.appendChild(text);
+console.log(h1);
+var main=document.getElementById("main");
+main.appendChild(h1);
