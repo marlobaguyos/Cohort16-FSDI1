64dbac3196cea87feec4e8eb917d5a7995e4ec7a
Marlo <marlo.baguyos@outlook.com>
2021-02-25 11:06:08 -0500

    udpate

--- 105/practice/practice.js
+++ 105/practice/practice.js
@@ -1,13 +1,16 @@
 //atributes
 var links = document.getElementsByTagName('a');
 console.log(links);
-links[0].setAttribute('target','blank');
-links[1].setAttribute('class','bordered');
+links[0].setAttribute('target', 'blank');
+links[1].setAttribute('class', 'bordered');
 
 //create html elements
-var h1=document.createElement('h1');
-var text=document.createTextNode("Hello World");
+var h1 = document.createElement('h1');
+var text = document.createTextNode("Hello World");
 h1.appendChild(text);
 console.log(h1);
-var main=document.getElementById("main");
+var main = document.getElementById("main");
 main.appendChild(h1);
+
+//hide an element
+main.style.display = "none";
