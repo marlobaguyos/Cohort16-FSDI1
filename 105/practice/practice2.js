6740a0f9058eedcbf70d560492dd075392904874
Marlo <marlo.baguyos@outlook.com>
2021-03-02 14:57:00 -0500

    update

--- 105/practice/practice2.js
+++ 105/practice/practice2.js
@@ -1,9 +1,46 @@
-console.log("Practice 2");
-//document.querySelector('main).display="none";
-jQuery('main').hide();
-jQuery('main').show();
-jQuery('nav a:fist').hide();
+console.log("test");
+//document.querySelector('main).style.display
+$('main').hide();
+$('main').show();
+$('nav a:first').hide();
 //hide the last input
-jQuery('form input:last').hide();
-var copy=$('main').clone();
-jQuery('main').append(copy);
+$('form input:last').hide();
+// var copy = $('main').clone();
+// $('main').append(copy);
+// $('header').prepend(copy);
+// $('h1').html("Hello");
+// var text = `<ul><li>Hello<li><ul>`;
+// $('#pets').append(text);
+
+$('h1').on('click' , function(){
+    console.log('Click..  ');
+});
+$('h1').on('mouseenter',function(){
+    console.log('mouse enters');
+});
+$('h1').on('mouseleave',function(){
+    console.log('mouse leaves');
+});
+
+var text = `
+<ul>
+    <li>Element 1</li>
+    <li>Element 2</li>
+    <li>Element 3</li>
+<ul>`;
+
+$("#pets").append(text);
+
+$("#pets").html("<ol> </ol>");
+for(var i=0;i<3;i++){
+    var li= $("<li></li>").text("Element" + i);
+    $("ol").append(li);
+}
+
+
+
+
+
+
+
+
