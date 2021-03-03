8d6883802d5e199c42db79f63ea12159957dbcde
Marlo <marlo.baguyos@outlook.com>
2021-03-02 21:51:11 -0500

    update

--- 105/practice/practice3.js
+++ 105/practice/practice3.js
@@ -1,6 +1,6 @@
 $('main').on({
     click:function(){
-        $(this).addClass('red-bg');
+        $(this).removeClass('active-test');
         $(this).addClass('red-bg');
     },
     mouseenter:function(){
@@ -9,4 +9,28 @@ $('main').on({
     mouseleave:function(){
         $(this).removeClass('active-test red-bg');
     }
+});
+
+var text = $('main h2').text();
+console.log(text);
+$('main h2').html('<h1> Hello World </h1>');
+//challenge: 
+//US: A user I want to click on the footer
+//and change the h2 text 
+
+$('footer').on({
+    click:function(){
+        $('this').removeClass('active-test');
+        $('this').addClass('red-bg');
+    },
+    mouseenter:function(){
+        $(this).addClass('active-test');
+    },
+    mouseleave:function(){
+        $(this).removeClass('active-test red-bg');
+    }
+});
+
+$('footer').on('click', function(){
+    $(this).text('this is the changed text');
 });
