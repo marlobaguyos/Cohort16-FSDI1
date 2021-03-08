c652b3d8bf193321cf01cc28f6570a8fbf5f2d14
Marlo <marlo.baguyos@outlook.com>
2021-03-07 23:27:11 -0500

    update

---
+++ 106/MANAGER/js/app.js
@@ -0,0 +1,41 @@
+task = [];
+var isItImportant = false;
+
+// Jump from One State to another State on and off - Bit 1 to 0 or to 0 to 1
+function toggleImportant(){
+    console.log("Icon clicked");
+
+    if(!isItImportant){
+        $("#iImportant").removeClass("far").addClass("fas");
+        isItImportant = true;
+    }else{
+        isItImportant = false;
+        $("#iImportant").removeClass("fas").addClass("far");
+    };
+}
+
+function saveTask(){
+    console.log("Saved clicked");
+}
+
+function init(){
+    console.log("Task Manager");
+
+    $("#iImportant").click(toggleImportant);
+    $("#btn-save").click(saveTask);
+}
+
+window.onload = init;
+
+
+/*
+Task
+- Id
+- Title
+- Date & time
+- Priority
+- Description
+- Status
+- Location
+- Color
+*/
