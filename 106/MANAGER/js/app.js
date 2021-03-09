f972590aad5e5741e7861040399cd2f24a1bdffb
Marlo <marlo.baguyos@outlook.com>
2021-03-09 09:22:14 -0500

    update

--- 106/MANAGER/js/app.js
+++ 106/MANAGER/js/app.js
@@ -36,17 +36,37 @@ function saveTask(){
 function displayTask(task){
     //create the syntax
 
-    var syntax = `<div class="card-body myTask">
-                    <h5 class="card-title">${task.title}</h5>
-                    <p class="card-text">Date: ${task.date}</p>
-                    <p class="card-text">${task.status}</p>
-                    <p class="card-text">${task.location}</p>
-                    <p class="card-text">${task.color}</p>
-                    <p class="card-text">${task.description}</p>
+    var syntax = `<div class="testBorder">
+                    <div class="d-flex bd-highlight testBorder">
+                        <div class="p-2 flex-fill bd-highlight testBorder">
+                        <i class="far fa-star task-star task-section-sm"></i>
+                        </div>
+
+                        <div class="p-2 flex-fill bd-highlight testBorder">
+                            <p class="testBorde">${task.title}</p>
+                            <p class="testBorde">${task.description}</p>
+                        </div>
+
+                        <div class="p-2 flex-fill bd-highlight testBorder">
+                            <div class="testBorde">${task.date}</div>
+                            <div class="testBorde">${task.location}</div>
+                            <div class="testBorde">${task.status}</div>
+                        </div>
+
+                        <div class="p-2 flex-fill bd-highlight testBorder">
+                        <i class="fas fa-dumpster" onclick="deleteTask()"></i>
+                        </div>
+                    </div>
                     </div>`
+
+                    
 
     //append the syntax to existing html
     $("#task-list").append(syntax);
+}
+
+function deleteTask(){
+    console.log("Should delete a task");
 }
 
 function init(){
